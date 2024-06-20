451d3f9db497f0a89ea8c737b8aea0aab1f90649
Musoxon Qaraxujayev <musoxonqaraxujayev@gmail.com>
2024-06-20 20:48:38 +0200

    removed prmpt results

--- client/serializers.py
+++ client/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
 from .models import ProUser
-from activities.serializers import ActivitySerializer, Activity  # Assuming you have ActivitySerializer defined
+from activities.serializers import ActivitySerializer, Activity
 
 class UserSerializer(serializers.ModelSerializer):
     activities = serializers.PrimaryKeyRelatedField(
